216ba8dfa109b5edf2866a9af8cce1df6debff29
adam <adam.thorpe.g@gmail.com>
2016-07-13 18:09:29 -0600

    Update build process.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 const gulp = require('gulp');
 const ava = require('gulp-ava');
+const xo = require('gulp-xo');
 
 gulp.task('test', () =>
-	gulp.src('test/test.js')
+	gulp.src('test/test*.js')
 		// gulp-ava needs filepaths so you can't have any plugins before it
 		.pipe(ava())
 );
+
+gulp.task('lint', () =>
+  gulp.src(['bin/**/*.js', 'lib/**/*.js'])
+    .pipe(xo())
+);
+
+gulp.task('default', ['lint', 'test']);
