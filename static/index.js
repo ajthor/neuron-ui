7a5f34da219b451e0ac99874d07d1ecdf2c8e5f2
adam <adam.thorpe.g@gmail.com>
2016-07-18 23:54:27 -0600

    Add Provider wrapper and 'store'.

--- static/index.js
+++ static/index.js
@@ -2,10 +2,31 @@
 
 const React = require('react');
 const ReactDOM = require('react-dom');
+const Redux = require('redux');
+const {Provider} = require('react-redux');
+
+const io = require('socket.io-client');
 // Deal with loading modules for react.
 const Workspace = require('../src/workspace.jsx');
 
+const socket = io('http://localhost:4195');
+
+socket.on('connect', () => {
+  socket.emit('message', 'CONNECTED. BEEP BOOP.');
+});
+
+//
+// Redux Reducers
+//
+const fileReducer = require('../src/file-reducer');
+//
+// Redux Store
+//
+const store = Redux.createStore(Redux.combineReducers({fileReducer}));
+
 ReactDOM.render(
-  <Workspace />,
+  <Provider store={store}>
+    <Workspace socket={socket} />
+  </Provider>,
   document.body
 );
